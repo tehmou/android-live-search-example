4387c5c8969115e56c34983947430caf04669053
tehmou <timo.tuominen@gmail.com>
2017-03-08 15:46:09 +0000

    Add solutions

--- app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.widget.RxCompoundButton;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,9 +19,17 @@ public class MainActivity extends AppCompatActivity {
         // Make the textView1 change text according to whether switchButton is "checked" or not
         Switch switchButton = (Switch) findViewById(R.id.switch_button);
         TextView textView1 = (TextView) findViewById(R.id.text_view_1);
+        
+        RxCompoundButton.checkedChanges(switchButton)
+                .subscribe(checked -> textView1.setText("Checked: " + checked));
+
 
         // Set the textView2 to say the text is too long if editText is more than 7 characters
         EditText editText = (EditText) findViewById(R.id.edit_text);
-        TextView textView2 = (TextView) findViewById(R.id.text_view_1);
+        TextView textView2 = (TextView) findViewById(R.id.text_view_2);
+
+        RxTextView.textChanges(editText)
+                .subscribe(text ->
+                        textView2.setText(text.length() > 7 ? "Text too long!" : ""));
     }
 }
