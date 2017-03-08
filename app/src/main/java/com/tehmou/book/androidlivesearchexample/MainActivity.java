32ae2499b44e73a449acc481950165df3fb5a2c7
tehmou <timo.tuominen@gmail.com>
2017-03-08 15:42:43 +0000

    Add UI controls

--- app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
@@ -2,6 +2,11 @@ package com.tehmou.book.androidlivesearchexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding.widget.RxCompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make the textView1 change text according to whether switchButton is "checked" or not
+        Switch switchButton = (Switch) findViewById(R.id.switch_button);
+        TextView textView1 = (TextView) findViewById(R.id.text_view_1);
+
+        // Set the textView2 to say the text is too long if editText is more than 7 characters
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        TextView textView2 = (TextView) findViewById(R.id.text_view_1);
     }
 }
